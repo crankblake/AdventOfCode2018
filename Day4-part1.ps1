cfb8038a5db2669d3fe435d8cc0143aad4bd46c7
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-11-25 21:26:20 -0700

    just basic refactor

--- Day4-part1.ps1
+++ Day4-part1.ps1
@@ -1,41 +1,20 @@
-<#
-if (Test-Path 'E:\Libraries\Documents\Scripting Projects\AdventOfCode\2018\Inputs')
+
+if (Test-Path 'E:\Libraries\Documents\Scripting Projects\AdventOfCode\2018\')
 {
-    Set-Location 'E:\Libraries\Documents\Scripting Projects\AdventOfCode\2018\Inputs'
+    Set-Location 'E:\Libraries\Documents\Scripting Projects\AdventOfCode\2018\'
 }else{
-    Set-Location 'E:\GameDevStuff\AdventOfCode2018\Inputs'
+    Set-Location 'E:\GameDevStuff\AdventOfCode2018\'
 }
-#>
 $arrayTotal = Get-Content .\Inputs\inputday4.txt | Sort-Object
-#$test.Substring(6,11) | Sort-Object 
 $arrayTime = $arrayTotal.Substring(6,11)
 $arrayDay = $arrayTotal.Substring(6,5) | Select-Object -Unique
 $arrayAction = @()
-$arrayGuard = @()
-#$array.Substring(19,9)
 foreach ($item in $arrayTotal){
     $len = $item.length
     $end = $len - 19
     #$item.Substring(19,$end)
     $arrayAction += $item.Substring(19,$end)
 }
-
-#$arrayGuard = ($arrayAction -match "Guard #\d\d\d\d").Substring(7,5)
-<#
-$arrayGuard = ($arrayAction -match "Guard #\d{3,4}").Substring(7,5)
-foreach ($item in $arrayTotal){
-    if ($item -match )
-}#>
-
-
-if ($arrayAction[$guardIndex] -match "Guard #\d\d\d\d"){
-    $guard = $arrayAction[$guardIndex].Substring(7,5)
-}else{
-    $guard = $arrayAction[$guardIndex].Substring(7,3)
-}
-
-#TODO: Create a custom object to store Guards and total time asleep
-
 #creates a custom object with an entry every day
 $Objects = $null
 foreach ($item in $arrayDay){
@@ -59,18 +38,15 @@ foreach ($item in $arrayDay){
         }else{
             $guard = $arrayAction[$guardIndex].Substring(7,3)
         }
-        
     }elseif($previousDayArray[$previousDayLastIndex] -match 'Guard')
         {
             #Write-Host "Found guard in yesterday's array
             $guardIndex = $arrayTotal.IndexOf($previousDayArray[$previousDayLastIndex])
-            #$guard = $arrayAction[$guardIndex].Substring(7,5)
             if ($arrayAction[$guardIndex] -match "Guard #\d\d\d\d"){
                 $guard = $arrayAction[$guardIndex].Substring(7,5)
             }else{
                 $guard = $arrayAction[$guardIndex].Substring(7,3)
             }
-
         }else{
             #Write-Error "Can't find guard for $($item)"
             $previousDayArray
@@ -107,8 +83,6 @@ foreach ($item in $arrayDay){
             Date = $item
             Guard = $guard
             Minutes = $minutes
-            #Time = $arrayTime.IndexOf($TimeSpaces[0])
-            #Time = ($arrayTime -match $item).IndexOf($arrayTime -match $item)
             Time = $minSleep
         })
     }else{
@@ -116,8 +90,6 @@ foreach ($item in $arrayDay){
             Date = $item
             Guard = $guard
             Minutes = $minutes
-            #Time = $arrayTime.IndexOf($TimeSpaces[0])
-            #Time = ($arrayTime -match $item).IndexOf($arrayTime -match $item)
             Time = 0    
         })
     }
@@ -131,38 +103,16 @@ foreach ($guard in $uniqueGuards){
     $totalSleep += $match.Time | Measure-Object -Sum
     #Write-Host "Total sleep for $($guard) is $(($match.Time | Measure-Object -Sum).Sum)"
 }
-#($totalSleep.Sum).Count
 $max = (($totalSleep.Sum) | Measure-Object -Maximum).Maximum
 $maxSleepIndex = $totalSleep.Sum.IndexOf($max)
 Write-Host "Guard $($uniqueGuards[$maxSleepIndex])has slept the most with $($max) minutes" -ForegroundColor Green
 $sleepiestGuardObject = $Objects -match $uniqueGuards[$maxSleepIndex]
-#TODO: calculate which minute he was most likely asleep
-#$minutesArray = [bool[]]::new(60)
-#$minutesArray.length
-<#
-$i = 0
-foreach ($element in $minutesArray){
-    Write-Host $i
-    Write-Host $minutesArray.IndexOf($element)
-    $minutesArray[$i] = $true
-    $i++
-}
-$minutesArray[40] = $true
-$minutesArray[0] = $false
-#>
-<#
-foreach($item in $sleepiestGuardObject.Date)
-{
-    $minutesObject += @([PSCustomObject]@{
-    Date = $item
-    Minutes = [bool[]]::new(60)   
-    })
-}#>
+
+#Calculate how often guard was asleep for each minute
 $finalArray = $null
 $finalArray = [int[]]::new(60)
 #$sleepiestGuardObject.Count
 foreach ($guardItem in $sleepiestGuardObject){
-#$guardItem
     for ($c = 0; $c -lt $guardItem.Minutes.Length; $c++){
         if ($guardItem.Minutes[$c]){
             $finalArray[$c]++
